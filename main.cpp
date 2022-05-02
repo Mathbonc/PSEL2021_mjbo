4518ee0b845ff1556e29c1c0d98f759f0b06592a
MatheusBonc <mjbo@cin.ufpe.br>
2022-05-02 00:44:51 -0300

    Robot orientation to the ball

--- main.cpp
+++ main.cpp
@@ -1,42 +1,116 @@
 #include <QCoreApplication>
+#include <QtMath>
 #include <chrono>
 #include <thread>
 #include <modules/actuator/actuator.h>
 #include <modules/vision/vision.h>
 
-class robotVel{
-public:
-    float vx;
-    float vy;
-    float vw;
-};
+int calculateQuadrant(float bx, float by, float rx, float ry){
+    int quad=0;
+    if((bx>rx)&&(by>ry)){//Primeiro Quadrante
+        quad = 1;
+    }else if((bx<rx)&&(by>ry)){//Segundo Quadrante
+        quad = 2;
+    }else if((bx<rx)&&(by<ry)){//Terceiro Quadrante
+        quad = 3;
+    }else if((bx>rx)&&(by<ry)){//Quarto Quadrante
+        quad = 4;
+    }else if((abs(bx-rx)<2)&&(by>ry)){//Diretamente em cima
+        quad = 11;
+    }else if((abs(by-ry)<2)&&(bx<rx)){//Diretamene na esquerda
+        quad = 12;
+    }else if((abs(bx-rx)<2)&&(by<ry)){//Diretamene em baixo
+        quad = 13;
+    }else if((abs(by-ry)<2)&&(by>ry)){//Diretament na direita
+        quad = 14;
+    }
+    return quad;
+}
 
-robotVel dummyVelChange(Vision *vision, int playerID,robotVel vector){
+float dummyOrientation(int quad,float bx, float by, float rx, float ry){
+    float desiredOrientation = M_PI,alpha;
+    qreal tg;
+
+    tg=(abs(bx-rx))/(abs(by-ry));
+    alpha = qAtan(tg);// Temos o alpha em radianos
+
+    switch (quad) {
+    case 1:
+        desiredOrientation = (M_PI_2 - alpha);
+        break;
+    case 2:
+        desiredOrientation = (M_PI_2 + alpha);
+        break;
+    case 3:
+        desiredOrientation = ((-M_PI_2) - alpha);
+        break;
+    case 4:
+        desiredOrientation = ((-M_PI_2) + alpha);
+        break;
+    case 11://Cima
+        desiredOrientation = M_PI_2;
+        break;
+    case 12://Esquerda
+        desiredOrientation = -M_PI;
+        break;
+    case 13://Baixo
+        desiredOrientation = -M_PI_2;
+        break;
+    case 14://Direita
+        desiredOrientation = 0.0;
+        break;
+    default:
+        break;
+    }
+
+    return desiredOrientation;
+}
+
+void dummyVelChange(Vision *vision, Actuator *actuator, int playerID){
     SSL_DetectionBall bola = vision->getLastBallDetection();
     SSL_DetectionRobot roboVision = vision->getLastRobotDetection(true, playerID);
+    int quad=0;
+    float desiredOrientation,vw;
 
-    //std::cout << bola.x() << " and " << roboVision.x() << std::endl;
-    //std::cout << roboVision.orientation() << std::endl;
+    quad = calculateQuadrant(bola.x(),bola.y(),roboVision.x(),roboVision.y());
+    desiredOrientation = dummyOrientation(quad,bola.x(),bola.y(),roboVision.x(),roboVision.y());
 
-    //X Position
-    if(bola.x()<roboVision.x()){//Direita da bola
-        vector.vx = 1.0;
-    }else if(bola.x()>roboVision.x()){//Esquerda da bola
-        vector.vx = -1.0;
-    }else {//Igual com a bola
-        vector.vx = 0.0;
+    //Testando vvvv
+    if(desiredOrientation == roboVision.orientation()){
+        vw = 0;
+    }else if(desiredOrientation > 0){//Bola está em cima
+        if(roboVision.orientation()>0){
+            if(roboVision.orientation()>desiredOrientation){
+                vw = -0.5;
+            }else{
+                vw = 0.5;
+            }
+        }else{
+            if(roboVision.orientation()<(-M_PI_2)){//Está no terceiro quad
+                vw = -0.5;
+            }else{
+                vw = 0.5;
+            }
+        }
+    }else{
+        if(roboVision.orientation()<0){
+            if(roboVision.orientation()>desiredOrientation){
+                vw = -0.5;
+            }else{
+                vw = 0.5;
+            }
+        }else{
+            if(roboVision.orientation()<M_PI_2){//Está no primeiro quad
+                vw = -0.5;
+            }else{
+                vw = 0.5;
+            }
+        }
     }
+    actuator->sendCommand(true,1,0,0,vw);
+    //Testando ^^^^
 
-    //Y Position
-    if(bola.y()<roboVision.y()){//Acima da bola
-        vector.vy = 1.0;
-    }else if(bola.y()>roboVision.y()){//Abaixo da bola
-        vector.vy = -1.0;
-    }else {//Igual com a bola
-        vector.vy = 0.0;
-    }
-
-    return vector;
+    printf("Quad: %d | desiredOrientation: %f | dummyOrientation: %f\n", quad, desiredOrientation, roboVision.orientation());
 }
 
 int main(int argc, char *argv[]) {
@@ -45,9 +119,6 @@ int main(int argc, char *argv[]) {
     Vision *vision = new Vision("224.5.23.2", 10020);
     Actuator *actuator = new Actuator("127.0.0.1", 20011);
 
-    int playerID = 3;
-    robotVel vector;
-
     // Desired frequency
     int desiredFrequency = 60;
 
@@ -55,14 +126,25 @@ int main(int argc, char *argv[]) {
         // TimePoint
         std::chrono::high_resolution_clock::time_point beforeProcess = std::chrono::high_resolution_clock::now();
 
-        // Process vision and actuator commands
+        // Process vision and actuator commands//
+
+
+
         vision->processNetworkDatagrams();
 
-        vector = dummyVelChange(vision,playerID,vector);
-        //actuator->sendCommand(true, 0, 0.0, 0.0, 0.25,false,3.0,true);
+        dummyVelChange(vision,actuator,1);
+
+        //SSL_DetectionRobot roboVision = vision->getLastRobotDetection(true, 2);
+        //SSL_DetectionBall bola = vision->getLastBallDetection();
+
+        //printf("(%f, %f)\n", bola.x(),bola.y());
+
+        //actuator->sendCommand(true,2,0,0,0);
         //actuator->sendCommand(true, playerID, vector.vx, vector.vy, 0.0,false,3.0,true);//X: Frente Y: Esquerda W:Vira esquerda
 
-        // TimePoint
+
+
+        // TimePoint//
         std::chrono::high_resolution_clock::time_point afterProcess = std::chrono::high_resolution_clock::now();
 
         // Sleep thread
