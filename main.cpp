9386487e3e7aeec277ed6b6651abd1fa1f916fb3
MatheusBonc <mjbo@cin.ufpe.br>
2022-05-04 14:53:43 -0300

    Quadrant and Orientation preliminar functions added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,85 @@
 #include <QCoreApplication>
+#include <QtMath>
 #include <chrono>
 #include <thread>
 #include <modules/actuator/actuator.h>
 #include <modules/vision/vision.h>
+
+int calculateQuadrant(float bx, float by, float rx, float ry){
+    int quad=0;
+    if((bx>rx)&&(by>ry)){//Primeiro Quadrante
+        quad = 1;
+    }else if((bx<rx)&&(by>ry)){//Segundo Quadrante
+        quad = 2;
+    }else if((bx<rx)&&(by<ry)){//Terceiro Quadrante
+        quad = 3;
+    }else if((bx>rx)&&(by<ry)){//Quarto Quadrante
+        quad = 4;
+    }else if((abs(bx-rx)<2)&&(by>ry)){//Diretamente em cima
+        quad = 11;
+    }else if((abs(by-ry)<2)&&(bx<rx)){//Diretamene na esquerda
+        quad = 12;
+    }else if((abs(bx-rx)<2)&&(by<ry)){//Diretamene em baixo
+        quad = 13;
+    }else if((abs(by-ry)<2)&&(by>ry)){//Diretament na direita
+        quad = 14;
+    }
+    return quad;
+}
+
+float dummyOrientation(int quad,float bx, float by, float rx, float ry){
+    float desiredOrientation = M_PI,alpha;
+    qreal tg;
+
+    tg=(abs(bx-rx))/(abs(by-ry));
+    alpha = qAtan(tg);// Temos o alpha em radianos
+
+    switch (quad) {
+    case 1:
+        desiredOrientation = (M_PI_2 - alpha);
+        break;
+    case 2:
+        desiredOrientation = (M_PI_2 + alpha);
+        break;
+    case 3:
+        desiredOrientation = ((-M_PI_2) - alpha);
+        break;
+    case 4:
+        desiredOrientation = ((-M_PI_2) + alpha);
+        break;
+    case 11://Cima
+        desiredOrientation = M_PI_2;
+        break;
+    case 12://Esquerda
+        desiredOrientation = -M_PI;
+        break;
+    case 13://Baixo
+        desiredOrientation = -M_PI_2;
+        break;
+    case 14://Direita
+        desiredOrientation = 0.0;
+        break;
+    default:
+        break;
+    }
+
+    return desiredOrientation;
+}
+
+void VSSS_Goal_Shot(Vision *vision, Actuator *actuator, bool isYellow, int playerID){
+    fira_message::Ball bola = vision->getLastBallDetection();
+    fira_message::Robot roboVision = vision->getLastRobotDetection(isYellow, playerID);
+    int quad=0;
+    float desiredOrientation,vw;
+
+    //Conseguindo as informações de orientação
+    quad = calculateQuadrant(bola.x(),bola.y(),roboVision.x(),roboVision.y());
+    desiredOrientation = dummyOrientation(quad,bola.x(),bola.y(),roboVision.x(),roboVision.y());
+
+
+    std::cout << quad << " and " << desiredOrientation << std::endl;
+    actuator->sendCommand(isYellow,playerID,0,0);//LW + | RW - : vira pra direita
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
@@ -19,7 +96,11 @@ int main(int argc, char *argv[]) {
 
         // Process vision and actuator commands
         vision->processNetworkDatagrams();
-        actuator->sendCommand(true, 0, 10.0, 10.0);
+
+        fira_message::Ball Bola = vision->getLastBallDetection();
+        fira_message::Robot Robo = vision->getLastRobotDetection(true, 1);
+
+        VSSS_Goal_Shot(vision,actuator,true,1);
 
         // TimePoint
         std::chrono::high_resolution_clock::time_point afterProcess = std::chrono::high_resolution_clock::now();
